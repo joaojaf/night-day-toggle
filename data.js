ce134ecb35b3bc62fce67cd26573493c299b0f51
ShinoBanshee <74556050+joaojaf@users.noreply.github.com>
2022-09-12 13:54:44 +0100

    4show
    its done alright

---
+++ data.js
@@ -0,0 +1,30 @@
+const articles = [
+  {
+    id: 1,
+    title: 'Jace the Mindsculptor',
+    date: new Date(2022, 2, 23),
+    length: 11,
+    snippet: `I'm baby ramps kombucha gluten-free ennui swag tattooed street art. Marfa biodiesel letterpress blue bottle subway tile, pop-up pok pok of.`,
+  },
+  {
+    id: 2,
+    title: 'Wrath of God',
+    date: new Date(2021, 12, 21),
+    length: 5,
+    snippet: `Biodiesel artisan seitan plaid sriracha copper mug venmo shabby chic. Kickstarter raclette kombucha, yr post-ironic jianbing try-hard flexitarian vaporware normcore.`,
+  },
+  {
+    id: 3,
+    title: 'Counterspell',
+    date: new Date(2020, 10, 11),
+    length: 5,
+    snippet: `Direct trade shabby chic four dollar toast, tilde actually try-hard bicycle rights aesthetic forage. Meditation keytar asymmetrical tacos artisan truffaut. Pabst jean shorts roof party scenester.`,
+  },
+  {
+    id: 4,
+    title: 'Lightning Bolt',
+    date: new Date(2019, 5, 3),
+    length: 5,
+    snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
+  },
+];
